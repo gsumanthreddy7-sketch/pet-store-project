ba7747ab5e1b6089d3c44a0f952a328815c05728
Sumanth Reddy <gsumanthreddy7@gmail.com>
2026-04-06 20:40:22 +0530

    fix render port

--- app.py
+++ app.py
@@ -540,5 +540,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed()
-    app.run(debug=True, port=5000)
+    import os
 
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
